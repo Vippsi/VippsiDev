e047c6abbb87aee75b91c567e5682bf09456955b
Jonathan <jonathanthornton1997@gmail.com>
2020-09-30 16:38:52 -0700

    updated bio

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,28 +1,30 @@
 import React from "react";
 const Body = () => {
   return (
-      <div className="body-div">
-          <div className='content-div'>
-            <div className='about-me-info'>
-                <div className='about-me-text'>
-                    <h3>About Me</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo id mi vel viverra. Vivamus faucibus convallis ipsum eu pulvinar. Sed gravida tortor sit amet commodo malesuada. Donec vitae risus at tortor consectetur lobortis. Aliquam non lacus tellus. Vestibulum vel risus ultricies, elementum orci vitae, lacinia augue. Sed nisl diam, cursus sed ligula lacinia, pharetra auctor dui. Nam ultrices hendrerit luctus. Donec id nunc ipsum. Mauris pharetra ligula non turpis tincidunt ultrices. Nunc mattis mauris vitae mi posuere vulputate. Etiam bibendum nulla sit amet placerat commodo. Phasellus bibendum odio eu erat euismod, sit amet dignissim ante ullamcorper.</p>
-                    </div> 
-                    
-                    <div className='skills'>
-                        <h3>Skills</h3>
-                        <p>HTML/CSS</p>
-                        <p>Javascript</p>
-                        <p>React</p>
-                    </div>  
-
-            </div>
-            
+    <div className="body-div">
+      <div className="content-div">
+        <div className="about-me-info">
+          <div className="about-me-text">
+            <h3>About Me</h3>
+            <p>
+              I am Full Stack web development student at Lambda School, who
+              loves technology, and what it can do in our day-to-day life. I'm
+              interested in working on ambitious projects, and working with
+              people!
+            </p>
           </div>
-          
+
+          <div className="skills">
+            <h3>Skills</h3>
+            <p>React || Redux</p>
+            <p>Node.js || Express</p>
+            <p>Javascript || HTML/CSS</p>
+            <p>Python</p>
+          </div>
+        </div>
       </div>
-      
-  )
+    </div>
+  );
 };
 
 export default Body;
